4e1d518693c0b7358dae42f87ee781833f01a9d2
gksrmf <smxlskan1500@naver.com>
2018-11-07 13:59:34 +0900

    20181107 hangeul commit 3;

--- Semi_Crojecter/src/com/kh/board/gallery/model/service/GalleryService.java
+++ Semi_Crojecter/src/com/kh/board/gallery/model/service/GalleryService.java
@@ -1,5 +1,26 @@
 package com.kh.board.gallery.model.service;
 
+import static com.kh.common.JDBCTemplate.close;
+import static com.kh.common.JDBCTemplate.getConnection;
+
+import java.sql.Connection;
+
+import com.kh.board.gallery.model.dao.GalleryDao;
+import com.kh.board.gallery.model.vo.GalleryForDetail;
+
 public class GalleryService {
+	
+	private GalleryDao gDao = new GalleryDao();
+
+	public GalleryForDetail selectOneGFD(int bid) {
+
+		Connection con = getConnection();
+		
+		GalleryForDetail gfd = gDao.selectOneGFD(con, bid);
+		
+		close(con);
+		
+		return gfd;
+	}
 
 }
